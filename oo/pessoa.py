3542011d2e11647244edcea1ffb94175aefa11d9
ikedartora <ikedaa>
2019-11-30 19:09:19 -0300

    Criados atributos de instância nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,9 +1,18 @@
-class Pessoa:                   # classe
-    def cumprimentar(self):     # método é uma 'função' atribuída a um classe
+class Pessoa:                                    # classe
+    def __init__(self, nome=None, idade=35):     # atributos de instância, criados através de __init__
+        self.idade = idade
+        self.nome = nome                         # o método self.nome retorna o parâmetro nome (por default None)
+
+    def cumprimentar(self):                      # método é uma 'função' atribuída a um classe
         return f'olá {id(self)}'
+                                                # Atributo de dado, instância e objeto são a mesma coisa
 
 if __name__ == '__main__':
-    p = Pessoa()
-    print(p.cumprimentar())
-    print(id(p))
+    p = Pessoa("Henrique")
     print(Pessoa.cumprimentar(p))
+    print(id(p))
+    print(p.cumprimentar())
+    print(p.nome)
+    p.nome = 'Ike'
+    print(p.nome)
+    print(p.idade)
