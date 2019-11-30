2f3320778e904fd2f86b2940edfc72c2c28e38dc
ikedartora <ikedaa>
2019-11-30 16:04:45 -0300

    Criado método cumprimentar

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,2 +1,9 @@
-class Pessoa:
-    pass
+class Pessoa:                   # classe
+    def cumprimentar(self):     # método é uma 'função' atribuída a um classe
+        return f'olá {id(self)}'
+
+if __name__ == '__main__':
+    p = Pessoa()
+    print(p.cumprimentar())
+    print(id(p))
+    print(Pessoa.cumprimentar(p))
