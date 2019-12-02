45a56cd5b0980ac8df02dfddafeae6d043543bfc
ikedartora <ikedaa>
2019-12-02 11:59:58 -0300

    Criado e removido atributo dinâmico

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,18 +1,24 @@
 class Pessoa:                                    # classe
-    def __init__(self, nome=None, idade=35):     # atributos de instância, criados através de __init__
+    def __init__(self, *filhos, nome=None, idade=35):     # atributos de instância, criados através de __init__  Atributo de dado, de instância e objeto são a mesma coisa
         self.idade = idade
         self.nome = nome                         # o método self.nome retorna o parâmetro nome (por default None)
-
-    def cumprimentar(self):                      # método é uma 'função' atribuída a um classe
+        self.filhos = list(filhos)
+    def cumprimentar(self):                      # método é uma 'função' atribuída a um classe, repare a diferença entre os atributos criados com init e sem, som init precisa de ()
         return f'olá {id(self)}'
-                                                # Atributo de dado, instância e objeto são a mesma coisa
+
 
 if __name__ == '__main__':
-    p = Pessoa("Henrique")
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = 'Ike'
-    print(p.nome)
-    print(p.idade)
+    renzo = Pessoa(nome='Renzo')
+    eu = Pessoa(nome='Eu')
+    luciano = Pessoa(renzo, eu, nome='Luciano')
+    print(Pessoa.cumprimentar(luciano))
+    print(id(luciano))
+    print(luciano.cumprimentar())
+    print(luciano.nome)
+    print(luciano.idade)
+    for filho in luciano.filhos:
+        print(filho.nome)
+    luciano.sobrenome = 'Ramalho'
+    del luciano.filhos
+    print(luciano.__dict__)
+    print(renzo.__dict__)
