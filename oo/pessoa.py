a59c042bb60d50e3c4236c26d3e0fce7f955d5e2
ikedartora <ikedaa>
2019-12-04 08:48:29 -0300

    Criado atributo de classe olhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,4 +1,5 @@
 class Pessoa:                                    # classe
+    olhos = 2       # atributos de classe, ocupa só 1 espaço na memória diferente dos de instância (1 para cada objeto criado)
     def __init__(self, *filhos, nome=None, idade=35):     # atributos de instância, criados através de __init__  Atributo de dado, de instância e objeto são a mesma coisa
         self.idade = idade
         self.nome = nome                         # o método self.nome retorna o parâmetro nome (por default None)
@@ -18,7 +19,11 @@ if __name__ == '__main__':
     print(luciano.idade)
     for filho in luciano.filhos:
         print(filho.nome)
-    luciano.sobrenome = 'Ramalho'
-    del luciano.filhos
-    print(luciano.__dict__)
+    luciano.sobrenome = 'Ramalho'       #atributo dinâmico criado em tempo de execução
+    del luciano.filhos                  # atribtuto dinâmico deletado em tempo de execução
+    print(luciano.__dict__)     #__dict__ mostra quais são os atributos de instância de um objeto
     print(renzo.__dict__)
+    print(Pessoa.olhos)
+    print(luciano.olhos)
+    print(renzo.olhos)
+    print(id(Pessoa.olhos), id(luciano.olhos), id(renzo.olhos))  #atributo de classe é o mesmo, ocupa só 1 espaço na memória
